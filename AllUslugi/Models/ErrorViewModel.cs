90e14ebb10be7345c75ad2dc6e06a25724d0b8d9
amina-2007 <aminagatagova@mail.ru>
2026-04-14 09:17:29 +0300

    Добавьте файлы проекта.

---
+++ AllUslugi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AllUslugi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
